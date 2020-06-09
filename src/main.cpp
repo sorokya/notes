99ae2e1635ac9b2b4a6e6c8dd740da8350672b44
Richard Leek <richard@richardleek.com>
2020-06-09 13:41:21 -0400

    added help info

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <string>
+#include <array>
 #include "NotesConfig.h"
 
-int main()
+void print_help();
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Version " << Notes_VERSION_MAJOR << "."
-              << Notes_VERSION_MINOR << std::endl;
+    if (argc < 2)
+    {
+        print_help();
+        return 1;
+    }
+ 
     return 0;
+}
+
+void print_help()
+{
+    std::cout << "Notes Version " << Notes_VERSION_MAJOR << "."
+              << Notes_VERSION_MINOR << std::endl;
+    
+    std::cout << "usage: notes <command> [<args>]" << std::endl;
+
+    std::array<std::string, 3> commands;
+    std::array<std::string, 3> descriptions;
+
+    commands[0] = "new";
+    descriptions[0] = "   Create a new note file";
+
+    commands[1] = "search";
+    descriptions[1] = "Search through notes";
+
+    commands[2] = "list";
+    descriptions[2] = "  Print out a list of all notes";
+
+    for (int i = 0; i < 3; i += 1)
+    {
+        std::cout << "   " << commands[i]
+                  << "  " << descriptions[i]
+                  << std::endl;
+    }
 }
